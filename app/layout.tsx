add82041542fb47cea8d51d17a63be7d9c1fb998
Tom Maksimovic <115001855+TomasMaksy@users.noreply.github.com>
2025-03-17 10:44:04 +0200

    Trying to get the search google going

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<head className="dark">
+			<head>
 				<link rel="icon" href="/favicon.ico" />
+				<title>Growth Meets Capital</title>
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
